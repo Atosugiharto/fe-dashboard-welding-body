70a68bfa6058fd6c87ee8a3c742fd29d2046f272
atosugiharto <atosoegiharto@gmail.com>
2025-01-26 14:31:35 +0700

    fix(sidebar): fullscreen all page

--- src/slices/sidebarSlice.js
+++ src/slices/sidebarSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const sidebarSlice = createSlice({
   name: "sidebar",
   initialState: {
-    hideSidebar: false,
+    hideSidebar: true,
   },
   reducers: {
     toggleSidebar: (state) => {
